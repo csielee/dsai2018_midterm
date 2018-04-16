fcb7604644bf0793de791b45198eeff3599affbe
李東霖 <lee097019@gmail.com>
2018-04-16 23:23:49 +0800

    Make submition more functional

--- api_submit.py
+++ api_submit.py
@@ -2,14 +2,27 @@
 # coding: utf-8
 
 import requests
-from config import data 
+from config import data
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--answer',
+                       default='answer.csv',
+                       help='answer csv file')
+parser.add_argument('--name',
+                        default='answer.csv',
+                        help='upload file name')
+parser.add_argument('--desc',
+                        default='this is my answer',
+                        help='description on web')
+args = parser.parse_args()
 
 filename = 'answer.csv'
 
-files={'files': open(filename,'rb')}
+files={'files': open(args.answer,'rb')}
 
-data["filename"] = filename
-data["description"] = 'this is my answer'
+data["filename"] = args.name
+data["description"] = args.desc
 
 url = 'https://biendata.com/competition/kdd_2018_submit/'
 
