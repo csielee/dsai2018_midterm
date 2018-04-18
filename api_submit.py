34da3c328b677184011d621b01a2ad69f9b7393a
李東霖 <lee097019@gmail.com>
2018-04-18 17:30:29 +0800

    Modify api_submit to be imported

--- api_submit.py
+++ api_submit.py
@@ -5,29 +5,32 @@ import requests
 from config import data
 import argparse 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--answer',
-                       default='answer.csv',
-                       help='answer csv file')
-parser.add_argument('--name',
+def sendSubmission(answerfile, name='answer.csv', desc=''):
+    files={'files': open(answerfile,'rb')}
+
+    data["filename"] = name
+    data["description"] = desc
+
+    url = 'https://biendata.com/competition/kdd_2018_submit/'
+
+    response = requests.post(url, files=files, data=data)
+
+    return response.text
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--answer',
                         default='answer.csv',
-                        help='upload file name')
-parser.add_argument('--desc',
-                        default='this is my answer',
-                        help='description on web')
-args = parser.parse_args()
+                        help='answer csv file')
+    parser.add_argument('--name',
+                            default='answer.csv',
+                            help='upload file name')
+    parser.add_argument('--desc',
+                            default='this is my answer',
+                            help='description on web')
+    args = parser.parse_args()
 
-filename = 'answer.csv'
+    print(sendSubmission(args.answer, args.name, args.desc))
 
-files={'files': open(args.answer,'rb')}
-
-data["filename"] = args.name
-data["description"] = args.desc
-
-url = 'https://biendata.com/competition/kdd_2018_submit/'
-
-response = requests.post(url, files=files, data=data)
-
-print(response.text)
 
 
